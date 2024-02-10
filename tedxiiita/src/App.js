b12663fcf33d8d432a64ae7ac14d4762a8bd1ee9
Rishi Upadhyay <rupadhyay2674@gmail.com>
2024-02-10 11:16:12 +0530

    spinner added

--- tedxiiita/src/App.js
+++ tedxiiita/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Spinner from './Components/Spinner/Spinner';
 
 function App() {
   return (
-    <div className="text-2xl text-red-400">
-      <h1>hello world</h1>
+    <div>
+      <Spinner/>
     </div>
   );
 }
